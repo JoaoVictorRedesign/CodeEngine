0aec042fe816560edf341ae71547c3cfbd681218
JoaoVictor <joao.victor@redesignconsultoria.com.br>
2024-03-06 16:15:34 -0300

    code changed

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express")
 const axios = require("axios")
 
-function main(args) {
-	const body = {
-	  args,
-	  env: process.env,
-	};
-  
-	console.log(`Return body: ${JSON.stringify(body, null, 2)}`);
-  
-	return {
-	  statusCode: 200,
-	  headers: { 
-		'Content-Type': 'application/json', 
-	  },
-	  body: "Ola mundo",
-	};
-  }
-  
-  module.exports.main = main;
+
+console.log("Ola mundo")
+  // Importa o módulo express
+
+// Cria uma instância do express
+const app = express();
+
+// Define uma rota básica
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
+// Define a porta em que o servidor irá escutar
+PORT = 8080;
+
+// Inicia o servidor na porta especificada
+app.listen(PORT, () => {
+  console.log(`Servidor está ouvindo na porta ${PORT}`);
+});
+
   
